11732138be78f1bb6b46bdc9e6c5468d60c95542
Shahriar Rabby <and.rabby5@gmail.com>
2024-11-11 00:02:09 +0600

    initial  commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,19 +8,17 @@
   </head>
   <body>
     {{-- Products card starte here  --}}
-    <div class="bd-example m-0 border-0">
-    
-        <div class="row  row-cols-1 row-cols-md-2 g-4">
-          <div class="col">
-            <div class="card">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
+    <div class="container d-flex justify-content-center">
+        <div class="card" style="width: 18rem;">
+            <img src="{{ asset($product->image) }}" class="card-img-top" alt="Product Image" style="height: 200px; object-fit: cover;">
+            <div class="card-body text-center">
+                <h5 class="card-title">{{ $product->name }}</h5>
+                <p class="card-text">Price: ${{ $product->price }}</p>
+                <a href="" class="btn btn-primary">Buy Now</a>
+                <a href="" class="btn btn-secondary">Add to Cart</a>
             </div>
-          </div>
+        </div>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
