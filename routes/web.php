ba2618c49478b09dcf2be6888e7bbcb65f50902d
Shahriar Rabby <and.rabby5@gmail.com>
2024-11-10 23:59:55 +0600

    initial  commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,4 @@ Route::get('/products/{id}/edit', [ProductsController::class, 'edit'])->name('pr
 Route::put('/products/{id}', [ProductsController::class, 'update'])->name('products.update');
 
 Route::get('/products/{id}',[ProductsController::class, 'delete'])->name('product.delete');
-Route::get('/single/product',[ProductsController::class, 'view'])->name('product.single');
-
-
-
+Route::get('/single/{id}',[ProductsController::class, 'view'])->name('product.single');
